11beed59483b56c6bc6ec24cefa081ac1a6c2bab
bogdanpantelimon <bogdanpantelimon98@gmail.com>
2023-02-02 00:21:53 +0200

    REST service for library first commit

---
+++ src/main/java/com/bogdanlibraryproject/bookrestservice/BookrestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bogdanlibraryproject.bookrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrestserviceApplication.class, args);
+	}
+
+}
